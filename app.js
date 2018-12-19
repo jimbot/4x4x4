f5f7fdf6c098dbbe81d3af0194649bc29a89d7cb
James Chen <jameschen1295@gmail.com>
2018-12-18 17:17:21 -0800

    added landing page

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+// public stores our css files
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+  res.render("landing");
+});
+
+app.listen(3000, function(){
+  console.log("listening on localhost: 3000");
+});
